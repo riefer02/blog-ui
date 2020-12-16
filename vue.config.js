c6d3292d13ed5b9838e82b9d3b33cb431ccea9aa
riefer02 <andrew.riefenstahl@gmail.com>
2020-12-15 18:50:17 -0600

    fix config module

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,3 @@
-const path =require('path');
+const path = require('path');
 
-module.exports = {
-  configureWebpack: {
-    outputDir: path.resolve(__dirname, '../blog-api/public'),
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:6969'
-        }
-      }
-    },
-  }
-};
+module.exports = {};
