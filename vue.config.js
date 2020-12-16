654cf8f2c83c04dc888443a95d2c5ffb005f6237
riefer02 <andrew.riefenstahl@gmail.com>
2020-12-15 18:46:16 -0600

    vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path =require('path');
+
 module.exports = {
   configureWebpack: {
+    outputDir: path.resolve(__dirname, '../blog-api/public'),
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:6969'
+        }
+      }
+    }
     plugins: []
   }
 };
