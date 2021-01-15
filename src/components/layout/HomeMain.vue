e6817d96b70a57e79be81951e9e86c8a0a7e5965
riefer02 <andrew.riefenstahl@gmail.com>
2021-01-14 19:05:10 -0600

    subtle smooth scroll

--- src/components/layout/HomeMain.vue
+++ src/components/layout/HomeMain.vue
@@ -5,26 +5,34 @@
       <hr />
       <p class="main-text">
         Hi, and welcome to my portfolio website. My name is Andrew Riefenstahl
-        and I am a full stack developer. A few years ago I began studying computer science and
-        I discovered a passion I never knew existed. I'm creative, love to
-        learn, and enjoy solving puzzles. When I'm not coding, I have many other
-        hobbies to explore, so if you want to learn more please, "stay awhile and
-        listen".
+        and I am a full stack developer. A few years ago I began studying
+        computer science and I discovered a passion I never knew existed. I'm
+        creative, love to learn, and enjoy solving puzzles. When I'm not coding,
+        I have many other hobbies to explore, so if you want to learn more
+        please, "stay awhile and listen".
       </p>
-         <p class="main-text">
-       The purpose of this website is to showcase different aspects of design and functionality I can bring to your projects or team. This site will be under constant development as I build out new features, and explore the awesome ecosystem that is the web.
+      <p class="main-text">
+        The purpose of this website is to showcase different aspects of design
+        and functionality I can bring to your projects or team. This site will
+        be under constant development as I build out new features, and explore
+        the awesome ecosystem that is the web.
       </p>
-          <p class="main-text">
-            Please create a user account and leave me a message on the <router-link class="inline-text-link" to="/blogs">blog page</router-link> so I can meet you and hear your feedback.
+      <p class="main-text">
+        Please create a user account and leave me a message on the
+        <router-link class="inline-text-link" @click="scrollToTop" to="/blogs"
+          >blog page</router-link
+        >
+        so I can meet you and hear your feedback.
       </p>
       <div class="full-bleed giraffe-banner">
         <ImagePanel image="giraffeHerd" alt="a picture of giraffes" />
       </div>
       <p class="main-text pt-5">
-        I like to explore many fields related to the human experience. I am a rock and roll music producer,
-         a writer, an anthropolgist, and a dog dad. One thing
-        I learned from a young age is, "It's the little things that add up to a great life."
-        So I try to make the best by exploring new ideas with each new day.
+        I like to explore many fields related to the human experience. I am a
+        rock and roll music producer, a writer, an anthropolgist, and a dog dad.
+        One thing I learned from a young age is, "It's the little things that
+        add up to a great life." So I try to make the best by exploring new
+        ideas with each new day.
       </p>
       <hr />
     </main>
@@ -76,6 +84,11 @@ export default {
       },
     ],
   }),
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  },
 };
 </script>
 
@@ -117,7 +130,7 @@ export default {
 
 .inline-text-link {
   text-decoration: none;
-  color:inherit;
+  color: inherit;
   font-weight: 600;
   font-style: italic;
 }
